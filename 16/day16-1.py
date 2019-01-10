3b6c9caf2a12cb183ff48c6011af5eab9e917b6b
Rickard <rickard>
2019-01-10 21:31:55 +0100

    dag 16

---
+++ 16/day16-1.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+from sys import argv
+from re import compile
+from re import findall
+
+def main():
+  pattern = compile('-?\d+')
+  opcodes=[addr,addi,mulr,muli,banr,bani,borr,bori,setr,seti,gtir,gtri,gtrr,eqir,eqri,eqrr]
+  svar=0
+  with open(argv[1]) as f:
+    input=f.read().strip().split('\n\n\n')
+  samples=input[0].split('\n\n')
+  samples=[s.split('\n') for s in samples]
+  samples=[[list(map(int,findall(pattern,x))) for x in s] for s in samples]
+#  examples=input[1]
+
+  for s in samples:
+    couldbe=0
+    for opcode in opcodes:
+      if opcode(s[0],s[1][1],s[1][2],s[1][3])==s[2][s[1][3]]:
+        couldbe+=1
+    if couldbe>2:
+      svar+=1
+  print(svar)
+
+# Operations
+def addr(r,a,b,c):
+  return r[a]+r[b]
+    
+def addi(registers,a,b,c):
+  return registers[a]+b
+
+def mulr(registers,a,b,c):
+  return registers[a]*registers[b]
+
+def muli(registers,a,b,c):
+  return registers[a]*b
+
+def banr(registers,a,b,c):
+  return registers[a]&registers[b]
+
+def bani(registers,a,b,c):
+  return registers[a]&b
+
+def borr(registers,a,b,c):
+  return registers[a]|registers[b]
+
+def bori(registers,a,b,c):
+  return registers[a]|b
+
+def setr(registers,a,b,c):
+  return registers[a]
+
+def seti(registers,a,b,c):
+  return a
+
+def gtir(registers,a,b,c):
+  return 1 if a>registers[b] else 0
+
+def gtri(registers,a,b,c):
+  return 1 if registers[a]>b else 0
+
+def gtrr(registers,a,b,c):
+  return 1 if registers[a]>registers[b] else 0
+
+def eqir(registers,a,b,c):
+  return 1 if a==registers[b] else 0
+
+def eqri(registers,a,b,c):
+  return 1 if registers[a]==b else 0
+
+def eqrr(registers,a,b,c):
+  return 1 if registers[a]==registers[b] else 0
+
+if __name__ == '__main__':
+  main()

ca30f5c03ec8701172c734462a9af5af446d4690
Rickard <rickard>
2019-01-10 21:30:23 +0100

    dag 9

---
+++ 09/day9-2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from sys import argv
+from re import findall
+from collections import deque
+
+def main():
+  with open(argv[1]) as f:
+    input=[[int(y) for y in findall(r'\d+', x)] for x in f]
+  players,max_marble,facit=input[0][:3] if len(input[0])>2 else input[0][:2]+[None]
+  max_marble=max_marble*100
+  marbles=deque([0])
+  score=list(0 for _ in range(players))
+  for x in range(1,max_marble+1): # kula 0 är instoppad vid skapande av kön
+    if x%23:
+      marbles.rotate(1)
+      marbles.appendleft(x)
+    else:
+      marbles.rotate(-7)
+      score[x%players]+=x+marbles.popleft()
+      marbles.rotate(1)
+  svar=max(score)
+  print(svar)
+  if facit:
+    if facit == svar:
+      print("Vilket stämmer med facitet")
+    else:
+      print("Stämmer ej med facit: "+str(facit))
+
+
+if __name__ == '__main__':
+  main()
